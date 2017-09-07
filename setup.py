2f3b093d6430ee69c0374f93ad0ff556844d65c6
Paul Hoffman <phoffman@nygenome.org>
2017-09-07 17:06:23 -0400

    Update setup.py with stuff for Tkinter, Cython, and extra metadata

--- setup.py
+++ setup.py
@@ -2,19 +2,44 @@
 
 """Setup CRISPRonto"""
 
+#   Deal with Tkinter stuff
+#   Needed for matplotlib
+#   However, not on Pip
+import sys
+try:
+    if sys.version_info.major == 2:
+        import Tkinter
+    elif sys.version_info.major == 3:
+        import tkinter
+    else:
+        sys.exit("Unsupported Python version")
+except ImportError as error:
+    sys.exit("Please install Tkinter and Tcl/Tk: " + str(error))
+
+
+#   Get stuff from setuptools
 from setuptools import setup
 from setuptools.extension import Extension
 from setuptools.command.install import install as _install
-from Cython.Distutils import build_ext
+
+#   Ensure Cython is available
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    import pip
+    pip.main(['install', 'cython'])
+    from Cython.Distutils import build_ext
+
 
 #   Some basic information
 NAME = 'CRISPRonto'
-VERSION = '1.0'
+VERSION = '1.0.0'
 AUTHOR = 'Alexandre Yahi'
+AUTHOR_EMAIL = 'ay2318@cumc.columbia.edu'
 DESCRIPTION = ''
 LICENSE = ''
-KEYWORDS = ''
-URL = ''
+KEYWORDS = 'crispr rnaseq'
+URL = 'https://github.com/lappalainenlab/crispronto'
 
 #   A class to force install to run build first
 class install(_install):
@@ -25,14 +50,39 @@ class install(_install):
         _install.run(self)
 
 
-#   Set up the extension
-EXT_MODULES = [
-    Extension(
-        name='crispronto.nw_align',
-        sources=['src/nw_align.pyx', 'src/py_align.cpp'],
-        language='c++'
-    )
+#   Classifiers
+CLASSIFIERS = [
+    #   See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    #   For more classfiiers
+    #   How mature is this project? Common values are
+    'Development Status :: 5 - Production/Stable',
+    #   Indicate who your project is intended for
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+    'Topic :: Scientific/Engineering :: Visualization',
+    #   Language
+    'Natural Language :: English',
+    'Topic :: Software Development :: Build Tools',
+    #   Pick your license as you wish (should match "license" above)
+    # 'License :: OSI Approved :: MIT License',
+    #   Specify the Python versions you support here. In particular, ensure
+    #   that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: C++',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: Implementation :: CPython',
 ]
+
+#   Specify Python version
+#   We support Python 2.7 and 3.5 or higher
+PYTHON_REQUIRES='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4'
+
+#   Platforms
+# PLATFORMS = ['Windows', 'Linux', 'Mac OS-X', 'UNIX']
+PLATFORMS = ['Linux', 'Mac OS-X', 'UNIX']
 
 #   Dependencies
 INSTALL_REQUIRES = [
@@ -48,6 +98,15 @@ INSTALL_REQUIRES = [
 PACKAGE_DIR = 'crispronto'
 PACKAGES = [
     PACKAGE_DIR
+]
+
+#   Set up the extension
+EXT_MODULES = [
+    Extension(
+        name='crispronto.nw_align',
+        sources=['src/nw_align.pyx', 'src/py_align.cpp'],
+        language='c++'
+    )
 ]
 
 #   Commands available for setup.py
@@ -68,11 +127,15 @@ setup(
     name=NAME,
     version=VERSION,
     author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
     description=DESCRIPTION,
     license=LICENSE,
     url=URL,
-    packages=PACKAGES,
+    platforms=PLATFORMS,
+    python_requires=PYTHON_REQUIRES,
+    classifiers=CLASSIFIERS,
     install_requires=INSTALL_REQUIRES,
+    packages=PACKAGES,
     ext_modules=EXT_MODULES,
     entry_points=ENTRY_POINTS,
     cmdclass=CMD_CLASS
